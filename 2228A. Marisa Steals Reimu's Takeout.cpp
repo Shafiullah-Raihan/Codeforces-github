7948a4465ed9238d570f019712b2234af91833ed
Shafiullah-Raihan <raihanshafiullah@gmail.com>
2026-05-17 01:47:11 +0600

    contest

---
+++ 2228A. Marisa Steals Reimu's Takeout.cpp
@@ -0,0 +1,112 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define yes             cout<<"YES"<<'\n';
+#define no              cout<<"NO"<<'\n';
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+#define MAX(x)          *max_element(all(x));
+#define MIN(x)          *min_element(all(x));
+#define SUM(a)          accumulate(all(a),0LL);
+#define clz(x)          __builtin_clz(x) /// count leading zeros of 32 bits.
+                                         ///like 4=00000000....100(leading 29 ta)
+#define ctz(x)          __builtin_ctz(x) ///count trailing zeros                                         
+#define popcount(x)     __builtin_popcount(x)    ///count number of ones's(bits)
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+
+
+
+void raihan()
+{
+    ll n;
+    cin >> n;
+
+    vl arr(n);
+
+    ll zero = 0, one = 0, two = 0;
+
+    for(ll i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+
+        if(arr[i] == 0) zero++;
+        if(arr[i] == 1) one++;
+        if(arr[i] == 2) two++;
+    }
+
+    ll ans = zero;
+
+    ll mn = min(one, two);
+
+    ans += mn;
+
+    one -= mn;
+    two -= mn;
+
+    ans += (one / 3);
+    ans += (two / 3);
+
+    cout << ans << endl;
+}
+
+
+
+
+int main()
+{
+    bismillah;
+    ll T;
+    cin>>T;
+    for(ll tt=1;tt<=T;tt++)
+    {
+        raihan();
+    }
+
+
+    return 0;
+}
+
