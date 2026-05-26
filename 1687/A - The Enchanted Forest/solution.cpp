b2ccbe43fd6d0916e6c9f89f1838c84551bc3236
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:31:40 +0600

    Add The Enchanted Forest [A] from Codeforces

---
+++ 1687/A - The Enchanted Forest/solution.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdC++.h"
+using namespace std;
+typedef long long ll;
+int main()
+{
+	ios::sync_with_stdio(0);cin.tie(0);
+	int T;cin>>T;
+	while (T--)
+	{
+		int n,k,i;
+		cin>>n>>k;
+		vector<ll> s(n+1);
+		for (i=1;i<=n;i++) cin>>s[i],s[i]+=s[i-1];
+		if (k>=n)
+		{
+			cout<<s[n]+(k-1ll+k-n)*n/2<<'
+';
+		}
+		else
+		{
+			ll mx=s[k];
+			for (i=k+1;i<=n;i++) mx=max((ll)mx,s[i]-s[i-k]);
+			cout<<mx+k*(k-1ll)/2<<'
+';
+		}
+		
+	}
+}
