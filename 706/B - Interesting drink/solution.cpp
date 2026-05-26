314e0c3b4c9e018afd788815abbaca8e37808225
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:36:28 +0600

    Add Interesting drink [B] from Codeforces

---
+++ 706/B - Interesting drink/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+ 
+    
+    sort(a.begin(), a.end());
+ 
+    int q;
+    cin >> q; 
+ 
+    while (q--) {
+        int x;
+        cin >> x;
+ 
+        cout << (upper_bound(a.begin(), a.end(), x) - a.begin()) << endl;
+    }
+ 
+    return 0;
+}
