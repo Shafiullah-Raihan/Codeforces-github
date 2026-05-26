cf4c894c512685016c254a8d5a060ad7c8f8b4d5
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:37:11 +0600

    Add Points on Plane [B] from Codeforces

---
+++ 1809/B - Points on Plane/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+ 
+ 
+int main()
+{
+ 
+        int T;cin>>T;
+        while(T--)
+        {
+            ll n;cin>>n;
+            ll sq = sqrtl(n-1);
+            cout<< sq<<endl;
+        }
+ 
+  return 0;
+}
