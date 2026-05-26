1cabc45a892d356624550206baca116e9123d912
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:33:04 +0600

    Add Sushi for Two [A] from Codeforces

---
+++ 1138/A - Sushi for Two/solution.cpp
@@ -0,0 +1,83 @@
+///   ***   ---   ||         ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+ 
+ 
+int main()
+{
+    bismillah;
+    int n;
+    cin >> n;
+    vector<int> v(n + 1, 0);  
+    vector<int> freq(3, 0);  
+    int ans = 0;
+ 
+    for (int i = 1; i <= n; i++) {
+        cin >> v[i];
+    }
+ 
+    for (int i = 1; i <= n; i++) {
+        if (v[i] != v[i - 1]) {
+            freq[v[i]] = 0;  
+        } else {
+            freq[v[i]]++;
+            ans = max(ans, min(freq[1], freq[2]) * 2);  
+        }
+    }
+ 
+    cout << ans+2 << endl;  
+ 
+ 
+ 
+    return 0;
+}
+ 
