668be986d603239735d8d047490ae025f0b3015a
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:53:34 +0600

    Add Odd Queries [D] from Codeforces

---
+++ 1807/D - Odd Queries/solution.cpp
@@ -0,0 +1,101 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+ 
+void raihan()
+{
+ 
+    ll n,q,sum=0;
+    cin>>n>>q;
+    vl arr(n),prefixSum(n+1,0);
+    fr(i,0,n)
+    {
+        cin>>arr[i];
+        prefixSum[i+1]= arr[i]+prefixSum[i];
+        sum+=arr[i];
+    }
+    while(q--)
+    {
+        ll temp=sum;
+        ll left,right,k;
+        cin>>left>>right>>k;
+        temp = temp -( prefixSum[right]-prefixSum[left-1]) + k*(right-left+1);
+        if(temp % 2==1)
+        {
+            cout<< "YES"<<endl;
+        }
+        else{
+            cout<< "NO"<<endl;
+        }
+    }
+ 
+ 
+}
+ 
+ 
+ 
+ 
+int main()
+{
+    bismillah;
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        raihan();
+    }
+ 
+ 
+    return 0;
+}
+ 
