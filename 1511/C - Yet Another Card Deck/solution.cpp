b856487c97f1cecc7e5ffa1b6bbe709530542eef
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:46:24 +0600

    Add Yet Another Card Deck [C] from Codeforces

---
+++ 1511/C - Yet Another Card Deck/solution.cpp
@@ -0,0 +1,84 @@
+///   ***   ---   ||         ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+ 
+ 
+int main()
+{
+    bismillah;
+    ll n,q;
+    cin>>n>>q;
+    deque<ll>arr(n),ans;
+ 
+    vl query(q);
+ 
+    for(int i=0;i<n;i++) cin>>arr[i];
+    for(int i=0;i<q;i++) cin>>query[i];
+    for(int i=0;i<q;i++)
+    {
+        ll temp = query[i];
+        auto it = find(all(arr),temp);
+        ans.push_back(it-arr.begin()+1);
+        arr.erase(it);
+        arr.push_front(temp);
+    }
+    for(auto u: ans)
+    {
+        cout<< u<< " ";
+    }
+    nl;
+ 
+ 
+ 
+    return 0;
+}
+ 
