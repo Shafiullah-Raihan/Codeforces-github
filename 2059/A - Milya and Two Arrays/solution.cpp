aeb04b6f2546a195321cfd683bce5a5365878cf0
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:40:28 +0600

    Add Milya and Two Arrays [A] from Codeforces

---
+++ 2059/A - Milya and Two Arrays/solution.cpp
@@ -0,0 +1,112 @@
+///   ***   ---   |         ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+ 
+ 
+int main()
+{
+    bismillah;
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        ll n;
+        cin>>n;
+        vl arr1(n),arr2(n);
+ 
+        for(ll i=0;i<n;i++)
+        {
+            cin>>arr1[i];
+ 
+        }
+        for(ll i=0;i<n;i++)
+        {
+            cin>> arr2[i];
+ 
+        }
+        if(n == 3){
+            cout<< "NO"<<endl;
+           continue;
+        }
+        set<ll>st1(all(arr1)),st2(all(arr2)),arrSum;
+ 
+        if(st1.size() >1 && st2.size() >1){
+            cout<< "YES"<<endl;
+            continue;
+        }
+ 
+ 
+        for(int i=0;i<n;i++)
+        {
+            ll p = arr1[i] + arr2[i];
+            arrSum.insert(p);
+        }
+ 
+        if(arrSum.size() >=3)
+        {
+            cout<< "YES"<<endl;
+        }
+        else{
+            cout<< "NO"<<endl;
+        }
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+ 
+    return 0;
+}
+ 
