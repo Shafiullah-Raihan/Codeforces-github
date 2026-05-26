c774d0758aa3efbd8bac002c69b79e7493772ab4
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:55:14 +0600

    Add Points on Line [C] from Codeforces

---
+++ 252/C - Points on Line/solution.cpp
@@ -0,0 +1,94 @@
+///   ***   ---   |         ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+void raihan()
+{
+ 
+ 
+     ll n, d;
+    cin >> n >> d;
+ 
+    if (n < 3) {
+        cout << 0 << endl;
+        return ;
+    }
+ 
+    vector<ll> arr(n);
+    fr(i,0, n) cin >> arr[i];
+ 
+    sort(all(arr));
+ 
+    ll ans = 0;
+    for (ll i = 0; i < n; i++) {
+        auto up = upper_bound(arr.begin() + i, arr.end(), arr[i] + d) - 1;
+        ll x = up - (arr.begin() + i);
+        if (x >= 2) ans += (x * (x - 1)) / 2; /// je i position e achi erpor 2 ta choose kore koita combination banano jabe oitai.
+    }
+ 
+    cout << ans << endl;
+ 
+ 
+}
+ 
+ 
+ 
+int main()
+{
+    bismillah;
+    raihan();
+ 
+ 
+ 
+    return 0;
+}
+ 
