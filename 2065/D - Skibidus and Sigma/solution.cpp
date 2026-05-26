61c06631e8dbf43433af33f98efae54ef65d1c9e
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:46:15 +0600

    Add Skibidus and Sigma [D] from Codeforces

---
+++ 2065/D - Skibidus and Sigma/solution.cpp
@@ -0,0 +1,112 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+struct ArrayData {
+    ll sum;
+};
+ 
+void raihan()
+{
+ 
+    int n, m;
+        cin >> n >> m;
+ 
+        vector<ArrayData> arr(n);
+ 
+        ll constantPart = 0;
+ 
+        priority_queue<ll> maxHeap;
+ 
+        for (int i = 0; i < n; i++) {
+            ll running = 0;
+            ll sum_i = 0;
+            for (int j = 0; j < m; j++) {
+                int x;
+                cin >> x;
+                sum_i += x;
+                running += x;
+                constantPart += running; // Increment score directly
+            }
+            maxHeap.push(sum_i);
+        }
+ 
+        ll additional = 0;
+        int weight = n - 1;
+ 
+        while (!maxHeap.empty()) {
+            ll sum_i = maxHeap.top();
+            maxHeap.pop();
+            additional += (ll)(weight--) * sum_i;
+        }
+ 
+        long long ans = constantPart + m * additional;
+        cout << ans << "
+";
+ 
+}
+ 
+ 
+ 
+ 
+int main()
+{
+    bismillah;
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        raihan();
+    }
+ 
+ 
+    return 0;
+}
+ 
