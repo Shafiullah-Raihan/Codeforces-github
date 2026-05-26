e667d809e63fc74a3ff13150a89eace1187d2728
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-05-26 07:58:59 +0600

    Add Shoe Shuffling [B] from Codeforces

---
+++ 1691/B - Shoe Shuffling/solution.cpp
@@ -0,0 +1,112 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+ 
+void raihan()
+{
+ 
+    ll n;cin>>n;
+    vl arr(n+1),ans(n+1);
+    for(ll i=1;i<=n;i++) ans[i] = i;
+    for(ll i=1;i<=n;i++)
+    {
+        cin>>arr[i];
+ 
+    }
+    if(n==1){
+        cout<<-1<<endl;return;
+    }
+    ll cnt=1;
+    for(ll i=2;i<=n;i++)
+    {
+        if(arr[i] == arr[i-1])
+        {
+            cnt++;swap(ans[i],ans[i-1]);
+        }
+        else{
+            if(cnt==1)
+            {
+                cout<<-1<<endl;
+                return;
+            }
+            cnt=1;
+        }
+    }
+    if(cnt==1){
+         cout<<-1<<endl;return;
+    }
+    for(ll i=1;i<=n;i++)
+    {
+        cout<< ans[i]<<" ";
+    }
+    nl;
+ 
+}
+ 
+ 
+ 
+ 
+int main()
+{
+    bismillah;
+    ll T;
+    cin>>T;
+    for(ll tt=1;tt<=T;tt++)
+    {
+        raihan();
+    }
+ 
+ 
+    return 0;
+}
+ 
