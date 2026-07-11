3d402cf8b97bf38216046c213005d46086c3f766
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-07-11 23:42:12 +0600

    Add Letter [A] from Codeforces

---
+++ 14/A - Letter/solution.cpp
@@ -0,0 +1,101 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define yes             cout<<"YES"<<'
+';
+#define no              cout<<"NO"<<'
+';
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+#define MAX(x)          *max_element(all(x));
+#define MIN(x)          *min_element(all(x));
+#define SUM(a)          accumulate(all(a),0LL);
+#define clz(x)          __builtin_clz(x) /// count leading zeros of 32 bits.
+                                         ///like 4=00000000....100(leading 29 ta)
+#define ctz(x)          __builtin_ctz(x) ///count trailing zeros
+#define popcount(x)     __builtin_popcount(x)    ///count number of ones's(bits)
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+void raihan()
+{
+	
+	ll n,m;cin>>n>>m;
+	vector<string>a(n);
+	for(ll i=0;i<n;i++)cin>>a[i];
+	ll r1=n,r2=-1,c1=m,c2=-1;
+	for(ll i=0;i<n;i++){
+		for(ll j=0;j<m;j++){
+			if(a[i][j]=='*'){
+				r1=min(r1,i);
+				r2=max(r2,i);
+				c1=min(c1,j);
+				c2=max(c2,j);
+			}
+		}
+	}
+	for(ll i=r1;i<=r2;i++){
+		for(ll j=c1;j<=c2;j++)cout<<a[i][j];
+		cout<<"
+";
+	}
+	
+}
+ 
+int main()
+{
+    bismillah;
+    ll T=1;
+    ///cin>>T;
+    for(ll tt=1;tt<=T;tt++)
+    {
+        raihan();
+    }
+ 
+ 
+    return 0;
+}
