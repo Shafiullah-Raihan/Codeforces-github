c82c4a54c18d1d5f986c66cb8f277a3bb5756956
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-07-15 18:58:23 +0600

    Add Extra-terrestrial Intelligence [A] from Codeforces

---
+++ 36/A - Extra-terrestrial Intelligence/solution.cpp
@@ -0,0 +1,103 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define yes cout<<"YES"<<'
+';
+#define no cout<<"NO"<<'
+';
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+#define MAX(x) *max_element(all(x));
+#define MIN(x) *min_element(all(x));
+#define SUM(a) accumulate(all(a),0LL);
+#define clz(x) __builtin_clz(x)
+#define ctz(x) __builtin_ctz(x)
+#define popcount(x) __builtin_popcount(x)
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+int dx[] = { 0, 0, +1, -1, -1, +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+void raihan()
+{
+    int n, l1 = 0, l2 = 0, j = 0, t = 0;
+    string a;
+ 
+    cin >> n >> a;
+ 
+    for(int i = 0; i < a.size(); i++)
+    {
+        if(a[i] == '1')
+        {
+            l2 = l1;
+            l1 = 0;
+            j = i + 1;
+ 
+            while(a[j] != '1' && j < n)
+                l1++, j++;
+ 
+            if(l2 != l1 && a[j] == '1' && t == 1)
+            {
+                no
+                return;
+            }
+ 
+            t = 1;
+        }
+    }
+ 
+    yes
+}
+ 
+int main()
+{
+    bismillah;
+    file();
+ 
+    ll T = 1;
+    // cin >> T;
+ 
+    while(T--)
+    {
+        raihan();
+    }
+ 
+    return 0;
+}
