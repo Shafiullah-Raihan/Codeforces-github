715cf4d5435bbac2ca234b6c5009741588a4f577
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-08-05 17:50:57 +0600

    Add Find the Different Ones! [D] from Codeforces

---
+++ 1927/D - Find the Different Ones!/solution.cpp
@@ -0,0 +1,117 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define yes             cout<<"YES"<<'
+';
+#define no              cout<<"NO"<<'
+'
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+#define MAX(x)          *max_element(all(x));
+#define MIN(x)          *min_element(all(x));
+#define SUM(a)          accumulate(all(a),0LL);
+#define clz(x)          __builtin_clz(x) /// count leading zeros of 32 bits.
+                                         ///like 4=00000000....100(leading 29 ta)
+#define ctz(x)          __builtin_ctz(x) ///count trailing zeros
+#define popcount(x)     __builtin_popcount(x)    ///count number of ones's(bits)
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+ 
+int a,a1,b[200005];
+ 
+void raihan()
+{
+    int n;
+    cin>>n;
+ 
+    a=0;
+ 
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a1;
+ 
+        if(a1!=a)
+            b[i]=i;
+        else
+            b[i]=b[i-1];
+ 
+        a=a1;
+    }
+ 
+    int q,l,r;
+    cin>>q;
+ 
+    while(q--)
+    {
+        cin>>l>>r;
+ 
+        if(b[r]>l)
+            cout<<b[r]-1<<" "<<r;
+        else
+            cout<<"-1 -1";
+ 
+        cout<<endl;
+    }
+}
+ 
+int main()
+{
+    bismillah;
+ 
+    ll T=1;
+    cin>>T;
+ 
+    for(ll tt=1;tt<=T;tt++)
+    {
+        raihan();
+    }
+ 
+ 
+    return 0;
+}
