8469125590bada6975718cfbcaa4327ea131d0cf
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-08-06 11:12:21 +0600

    Add Range Update Point Query [F] from Codeforces

---
+++ 1791/F - Range Update Point Query/solution.cpp
@@ -0,0 +1,137 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define yes             cout<<"YES"<<'
+';
+#define no              cout<<"NO"<<'
+'
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+#define MAX(x)          *max_element(all(x));
+#define MIN(x)          *min_element(all(x));
+#define SUM(a)          accumulate(all(a),0LL);
+#define clz(x)          __builtin_clz(x) /// count leading zeros of 32 bits.
+                                         ///like 4=00000000....100(leading 29 ta)
+#define ctz(x)          __builtin_ctz(x) ///count trailing zeros
+#define popcount(x)     __builtin_popcount(x)    ///count number of ones's(bits)
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1,  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+ 
+int a[200020],b[200020];
+ 
+int find(int x)
+{
+    return b[x]==x ? x : b[x]=find(b[x]);
+}
+ 
+void raihan()
+{
+    int n,q;
+    cin>>n>>q;
+ 
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+        b[i]=i;
+    }
+ 
+    b[n+1]=n+1;
+ 
+    while(q--)
+    {
+        int op,l,r;
+        cin>>op>>l;
+ 
+        if(op==1)
+        {
+            cin>>r;
+ 
+            for(int i=l;i<=r;i++)
+            {
+                i=find(i);
+ 
+                if(i>r)
+                    break;
+ 
+                int ans=0;
+ 
+                while(a[i])
+                {
+                    ans+=a[i]%10;
+                    a[i]/=10;
+                }
+ 
+                a[i]=ans;
+ 
+                if(a[i]<10)
+                    b[i]=i+1;
+            }
+        }
+        else
+        {
+            cout<<a[l]<<endl;
+        }
+    }
+}
+ 
+int main()
+{
+    bismillah;
+ 
+    ll T=1;
+    cin>>T;
+ 
+    for(ll tt=1;tt<=T;tt++)
+    {
+        raihan();
+    }
+ 
+ 
+    return 0;
+}
