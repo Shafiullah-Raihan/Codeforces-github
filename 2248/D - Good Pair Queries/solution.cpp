1e23cd640055d08506a41a268c70433a92f6bac6
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-08-11 18:59:28 +0600

    Add Good Pair Queries [D] from Codeforces

---
+++ 2248/D - Good Pair Queries/solution.cpp
@@ -0,0 +1,112 @@
+///   ***   ---         ||    ALLAHU AKBAR        |||   ---   ***   ///
+ 
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define endl '
+'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define nl cout<<endl
+#define yes             cout<<"YES"<<'
+';
+#define no              cout<<"NO"<<'
+';
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+#define MAX(x)          *max_element(all(x));
+#define MIN(x)          *min_element(all(x));
+#define SUM(a)          accumulate(all(a),0LL);
+#define clz(x)          __builtin_clz(x)
+#define ctz(x)          __builtin_ctz(x)
+#define popcount(x)     __builtin_popcount(x)
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ 
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd(ll a,ll b){ return __gcd(a,b); }
+ll lcm(ll a,ll b){ return a*(b/gcd(a,b)); }
+ 
+ 
+int dx[] = { 0, 0, +1, -1, -1, +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+ 
+ 
+ll n,q,l,r;
+ll num[200009][2][2];
+string s,t;
+ 
+void raihan()
+{
+    cin>>n>>q>>s>>t;
+ 
+    for(int i=1;i<=n;i++)
+    {
+        for(int a:{0,1})
+            for(int b:{0,1})
+                num[i][a][b]=num[i-1][a][b];
+ 
+        num[i][s[i-1]-'0'][t[i-1]-'0']++;
+    }
+ 
+    while(q--)
+    {
+        cin>>l>>r;
+ 
+        ll c00=num[r][0][0]-num[l-1][0][0];
+        ll c01=num[r][0][1]-num[l-1][0][1];
+        ll c10=num[r][1][0]-num[l-1][1][0];
+        ll c11=num[r][1][1]-num[l-1][1][1];
+ 
+        ll x=max(c01,c10)-min(c01,c10);
+ 
+        if(c00+c11>=x)
+            yes
+        else
+            no
+    }
+}
+ 
+int main()
+{
+    bismillah;
+ 
+    ll T=1;
+    cin>>T;
+ 
+    for(ll tt=1;tt<=T;tt++)
+    {
+        raihan();
+    }
+ 
+    return 0;
+}
